85a6e9ee61bce6a4f7e82e2a9f0309c79ed4b20b
Nicola <niconix.dev@gmail.com>
2020-04-15 10:29:31 +0200

    Correzione

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,24 @@
 $(document).ready(function () {
     
-    var dropDown = $('.dropdown');
-    var hoverDrop = $('.with-dropdown');
     var arrow = $('.languages > i')
     var language = $('.languages')
 
-    hoverDrop.mouseenter(function(){
-
-        hoverDrop.not($(this)).next('.dropdown').hide();
-
-        $(this).next('.dropdown').show();
+    // hoverDrop.mouseenter(function(){
+    //     hoverDrop.not($(this)).next('.dropdown').hide();
+    //     $(this).children('.dropdown').show();
         
-    });
-
-    dropDown.mouseleave(function(){
-
-        hoverDrop.not($(this)).next('.dropdown').hide();
-    });
+    // });
+    // dropDown.mouseleave(function(){
+    //     hoverDrop.not($(this)).next('.dropdown').hide();
+    // });
     
+    $('.with-dropdown').mouseenter(function(){
+        $(this).children('.dropdown').addClass('active');
+    });
+
+    $('.with-dropdown').mouseleave(function(){
+        $(this).children('.dropdown').removeClass('active');
+    });
 
     language.mouseenter(function(){
         arrow.removeClass('fa-angle-down').addClass('fa-angle-up');
